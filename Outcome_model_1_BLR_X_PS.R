36e8d0ebd73d631e9b8d1acbd622d847a2ad11b3
Ruyi-Liu <83448837+Ruyi-Liu@users.noreply.github.com>
2024-06-20 09:01:50 -0400

    Add files via upload

---
+++ Outcome_model_1_BLR_X_PS.R
@@ -0,0 +1,321 @@
+
+library(SuperLearner)
+library(glmnet)
+library(purrr)
+library(boot)
+library(invgamma)
+library(coda)
+library(MASS)
+library(purrr)
+library(boot)
+library(extraDistr)
+library(mnormt)
+library(BayesTree)
+
+# BLR
+BLR_ICE_X_PS <- function(dat){
+  ###############
+  #Global Setting
+  ###############
+  mc_num <- 4000
+  burnin<- 3000
+  mc_num_stage_1 <- mc_num
+  # the 10th draw is kept
+  keepevery_stage2 <- 10
+  keepevery_stage1 <- 100
+  
+  n=dim(dat)[1]
+  
+  ############################# Stage 2 ###################################
+  X = cbind(1,dat$A1,dat$A2,dat$X_11,dat$X_21,dat$X_3,dat$L1,dat$X_12,dat$X_22,dat$L2)   
+  #X = cbind(1,dat$A1,dat$A2,dat$L1,dat$L2) 
+  y=dat$Y
+  
+  ###############
+  #Prior Settings
+  ###############
+  # delta_2
+  mean_delta_2_prior <- rep(0,dim(X)[2])
+  cov_delta_2_prior <- 10000*diag(dim(X)[2])
+  # sigma_2^2
+  a<-0.001
+  b<-0.001
+  
+  ############################
+  #Parameters
+  ############################
+  delta_2 <- matrix(0,nrow=mc_num,ncol=dim(X)[2])
+  sigma_2 <- rep(0, times=mc_num)
+  
+  ###############
+  #Initial Values
+  ###############
+  delta_2[1,] <- rep(1,dim(X)[2])
+  sigma_2[1] <- 1
+  
+  
+  Sigma_1_result <- c()
+  Delta_1_result <- matrix(nrow=0,ncol=7)
+  
+  
+  for(i in 2:mc_num){
+    ######################################################################
+    #Update delta_2
+    ######################################################################
+    mean_delta_2<- solve(solve(cov_delta_2_prior) + ((1/sigma_2[i-1])*(t(X)%*%X)))%*%
+      (solve(cov_delta_2_prior) %*% mean_delta_2_prior + ((1/sigma_2[i-1])*(t(X)%*%y)))
+    
+    cov_delta_2<-solve(solve(cov_delta_2_prior) + ((1/sigma_2[i-1])*(t(X)%*%X)))
+    
+    delta_2[i,]<-rmnorm(n=1,mean=mean_delta_2,varcov=cov_delta_2)
+    
+    ################################
+    #Update sigma_2
+    ################################
+    sigma_2[i] <- rinvgamma(1,n/2+a,t(dat$Y-as.matrix(X)%*%as.vector(delta_2[i,]))%*%
+                              (dat$Y-as.matrix(X)%*%as.vector(delta_2[i,]))/2+b)
+    
+  }
+  
+  
+  
+  #### Create new dataframe with sample size 2N using stage 2 posterior
+  rows_to_keep <- seq(from = burnin+keepevery_stage2, to = mc_num, by = keepevery_stage2)
+  parameters <- as.data.frame(delta_2[rows_to_keep,])
+  sigma_2_sub <- sigma_2[rows_to_keep]
+  
+  for(i in 1:length(sigma_2_sub)){
+    newdata <- dat
+    newdata$a2_0 <- 0
+    newdata$a2_1 <- 1
+    X_a2_0 <- as.matrix(cbind(1,newdata[,c("A1","a2_0","X_11","X_21","X_3","L1","X_12","X_22","L2")]))
+    X_a2_1 <- as.matrix(cbind(1,newdata[,c("A1","a2_1","X_11","X_21","X_3","L1","X_12","X_22","L2")]))
+    # find Y_a2_0
+    newdata$Y_a2_0 <- c(X_a2_0%*%t((parameters[i,])) +
+                          rnorm((dim(newdata)[1]),mean=0,sd=sqrt(sigma_2_sub[i])))
+    
+    # find Y_a2_1
+    newdata$Y_a2_1 <- c(X_a2_1%*%t((parameters[i,])) +
+                          rnorm((dim(newdata)[1]),mean=0,sd=sqrt(sigma_2_sub[i])))
+    
+    # Create augumented dataset
+    newdata0 <- newdata[,c(1:9,11,13)]
+    colnames(newdata0)[c(10,11)] <- c("a2","Y_hat")
+    newdata1 <- newdata[,c(1:9,12,14)]
+    colnames(newdata1)[c(10,11)] <- c("a2","Y_hat")
+    # change to long format
+    # Obtain D_aug 
+    data_aug <- as.data.frame(rbind(newdata0,newdata1))
+    
+    ################################################# Stage 1 ###########
+    X <- as.matrix(cbind(1,data_aug$A1,data_aug$a2,data_aug$X_11,
+                         data_aug$X_21,data_aug$X_3,data_aug$L1))
+    y <- data_aug$Y_hat
+    
+    ###############
+    #Prior Settings
+    ###############
+    
+    
+    a<-0.001
+    b<-0.001
+    
+    mean_delta_1_prior <- rep(0,dim(X)[2])
+    cov_delta_1_prior <- 10000*diag(dim(X)[2])
+    
+    
+    ############################
+    #Parameters
+    ############################
+    delta_1 <- matrix(0,nrow=mc_num_stage_1,ncol=dim(X)[2])
+    sigma_1 <- rep(0, times=mc_num_stage_1)
+    
+    ###############
+    #Initial Values
+    ###############
+    
+    delta_1[1,] <- rep(1,dim(X)[2])
+    sigma_1[1] <- 1
+    
+    for(j in 2:mc_num_stage_1){
+      ######################################################################
+      #Update delta_2
+      ######################################################################
+      
+      mean_delta_1 <- solve(solve(cov_delta_1_prior) +
+                              ((1/sigma_1[j-1])*(t(X)%*%X)))%*%
+        (solve(cov_delta_1_prior) %*% mean_delta_1_prior +
+           ((1/sigma_1[j-1])*(t(X)%*%y)))
+      
+      cov_delta_1<-solve(solve(cov_delta_1_prior) + ((1/sigma_1[j-1])*(t(X)%*%X)))
+      
+      delta_1[j,]<-rmnorm(n=1,mean=mean_delta_1,varcov=cov_delta_1)
+      
+      ################################
+      #Update sigma_1
+      ################################
+      sigma_1[j] <- rinvgamma(1,n+a,t(y-as.matrix(X)%*%as.vector(delta_1[j,]))%*%
+                                (y-as.matrix(X)%*%as.vector(delta_1[j,]))/2+b)
+      
+    }
+    
+    rows_to_keep_1 <- seq(from = burnin+keepevery_stage1, to = mc_num_stage_1, by = keepevery_stage1)
+    for (index_keep in rows_to_keep_1){
+      Sigma_1_result <- c(Sigma_1_result,sigma_1[index_keep])
+      Delta_1_result <- rbind(Delta_1_result, delta_1[index_keep,])
+    }
+    
+  }
+  
+  
+  ########################################### Sample y missing #####################
+  # define linear model 1
+  newdata_aug <- dat
+  newdata_aug$a1_0 <- 0
+  newdata_aug$a1_1 <- 1
+  newdata_aug$a2_0 <- 0
+  newdata_aug$a2_1 <- 1
+  parameters_1 <- as.data.frame(Delta_1_result)
+  sigma_1_sub <- Sigma_1_result
+  X_a1_0_a2_0 <- as.matrix(cbind(1,newdata_aug[,c("a1_0","a2_0","X_11","X_21","X_3","L1")]))
+  X_a1_1_a2_0 <- as.matrix(cbind(1,newdata_aug[,c("a1_1","a2_0","X_11","X_21","X_3","L1")]))
+  X_a1_0_a2_1 <- as.matrix(cbind(1,newdata_aug[,c("a1_0","a2_1","X_11","X_21","X_3","L1")]))
+  X_a1_1_a2_1 <- as.matrix(cbind(1,newdata_aug[,c("a1_1","a2_1","X_11","X_21","X_3","L1")]))
+  
+  res_a1_0_a2_0 <- list()
+  res_a1_1_a2_0 <- list()
+  res_a1_0_a2_1 <- list()
+  res_a1_1_a2_1 <- list()
+  ATE_list <- c()
+  for(i in 1:length(sigma_1_sub)){
+    ############# Remove error terms in the last step ###############
+    res_a1_0_a2_0[[i]] <- c(X_a1_0_a2_0%*%t((parameters_1[i,]))) 
+    # find Y_a1_1_a2_0
+    res_a1_1_a2_0[[i]] <- c(X_a1_1_a2_0%*%t((parameters_1[i,])))
+    # find Y_a1_0_a2_1
+    res_a1_0_a2_1[[i]] <- c(X_a1_0_a2_1%*%t((parameters_1[i,]))) 
+    # find Y_a1_1_a2_1
+    res_a1_1_a2_1[[i]] <- c(X_a1_1_a2_1%*%t((parameters_1[i,])))
+    ATE_list[i] <- mean(c(X_a1_1_a2_1%*%t((parameters_1[i,])))) - mean(c(X_a1_0_a2_0%*%t((parameters_1[i,]))))
+  }
+  #print(ATE_list)
+  print(length(ATE_list))
+  return(ATE_list)
+}
+
+
+
+
+M = 100  # number of datasets simulated: 50~500
+n = 500
+true_ATE_Linear <- -19.99898
+est_ATE <- c()
+sd_ATE <- c()
+coverage_num <- 0
+upper_CI <- 0
+lower_CI <- 0
+ATE_list <- c()
+
+for (i in 1:M){
+  seed_num <- 1000+i
+  set.seed(seed_num)
+  # At time t = 1, simulate for each individual baseline covariates X_11, X_21, X_3
+  X_11 <- rnorm(n,0,1)
+  X_21 <- rnorm(n,0,1)
+  X_3 <- abs(rnorm(n,0,1))
+  
+  # Generate treatment A_1
+  A0 <- rep(0, n)
+  pi1 <- (exp(-A0+ (1*X_11-0.5*X_21+0.25*X_3) + (-1/2)))/(1+exp(-A0+(1*X_11-0.5*X_21+0.25*X_3)+(-1/2)))
+  A1 <- rbern(n, prob = pi1)
+  
+  # At time t = 2, simulate for each individual their time-varying confounders X_12, X_22
+  U2 <- 2+(2*A1 - 1)/3
+  
+  Z12 <- rnorm(n,0,1)
+  Z22 <- rnorm(n,0,1)
+  
+  X_12 <- Z12*U2
+  X_22 <- Z22*U2
+  
+  # Generate treatment A_2
+  pi2 <- (exp(-A1 + (1*X_12-0.5*X_22) + (-1/2)^2))/(1 + exp(-A1+(1*X_12-0.5*X_22)+(-1/2)^2))
+  A2 <- rbern(n, prob = pi2)
+  
+  Y <- 250-10*A1-10*A2+27.4*X_11+13.7*X_21+13.7*X_3+27.4*X_12+13.7*X_22+rnorm(n,0,sd=5)
+  
+  # Calculate propensity score
+  # Stage 2
+  x_df_2 <- as.data.frame(cbind(X_11,X_21,X_3,A1,X_12,X_22))
+  sl_lib <- c("SL.glmnet", "SL.glm", "SL.gam")
+  result_2 <- SuperLearner(Y = A2, X = x_df_2, SL.library = sl_lib, method = "method.NNLS", family = binomial())
+  mydata_X_superlearner_predict <- predict(result_2)
+  e2 <- ifelse(A2 == 0, 1-as.numeric(mydata_X_superlearner_predict$pred),
+                                     as.numeric(mydata_X_superlearner_predict$pred))
+  L2 <- e2
+  
+  # Stage 1
+  x_df_1 <- as.data.frame(cbind(X_11,X_21,X_3))
+  result_1 <- SuperLearner(Y = A1, X = x_df_1, SL.library = sl_lib, method = "method.NNLS", family = binomial())
+  mydata_X_superlearner_predict <- predict(result_1)
+  e1 <- ifelse(A1 == 0, 1-as.numeric(mydata_X_superlearner_predict$pred),
+               as.numeric(mydata_X_superlearner_predict$pred))
+  L1 <- e1
+  
+  # Collect the propensity score and combine the dataset
+  dat <- as.data.frame(cbind(L1,A1,L2,A2,X_11,X_21,X_3,X_12,X_22,Y))
+  
+  
+  ATE_list <- BLR_ICE_X_PS(dat)
+  est_ATE[i] <- mean(ATE_list)
+  sd_ATE[i] <- sd(ATE_list)
+  upper_CI <- quantile(ATE_list, 0.975)
+  lower_CI <- quantile(ATE_list, 0.025)
+  
+  print("-----------Seed Number------------------")
+  print(seed_num)
+  
+  c <- 0
+  
+  if((true_ATE_Linear <= upper_CI) & (true_ATE_Linear >= lower_CI)){
+    coverage_num <- coverage_num+1
+    c <- 1
+  }
+  
+  print("-----------Individual Coverage------------------")
+  print(c)
+  print("--------------Individual ATE---------------")
+  print(est_ATE[i])
+  print("-----------------------------")
+}
+
+
+
+# Percentage Bias
+bias_BLR <- mean(est_ATE)-true_ATE_Linear
+percentage_bias_BLR <- abs(bias_BLR/true_ATE_Linear)
+paste("(BLR_Linear_X_PS) Percentage Bias: ", percentage_bias_BLR)
+
+# Credible Interval Coverage: the proportion of all datasets for which the 95% credible intervals contained the true ATE
+credible_interval_coverage_BLR <- coverage_num/M
+paste("(BLR_Linear_X_PS) Credible Interval Coverage: ", credible_interval_coverage_BLR)
+
+# RMSE
+BLR_RMSE <- sqrt(mean((est_ATE - true_ATE_Linear)^2))
+paste("(BLR_Linear_X_PS) RMSE: ", BLR_RMSE)
+
+# MCSE
+MCSE <- sd(est_ATE)
+paste("(BLR_Linear_X_PS) MCSE: ", MCSE)
+
+# AESE
+AESE <- mean(sd_ATE)
+paste("(BLR_Linear_X_PS) AESE: ", AESE)
+
+
+
+
+
+
+
+
---
+++ Outcome_model_2_BART_X_PS.R
@@ -0,0 +1,226 @@
+
+
+library(SuperLearner)
+library(glmnet)
+library(purrr)
+library(boot)
+library(invgamma)
+library(coda)
+library(MASS)
+library(purrr)
+library(boot)
+library(extraDistr)
+library(mnormt)
+library(BayesTree)
+
+BART_ICE_X_PS <- function(dat){
+  ###############
+  #Global Setting
+  ###############
+  mc_num <- 4000
+  burnin<- 3000
+  mc_num_stage_1 <- mc_num
+  # the 10th draw is kept
+  keepevery_stage2 <- 10
+  keepevery_stage1 <- 100
+  
+  # mc_num <- 300
+  # burnin<- 100
+  # mc_num_stage_1 <- mc_num
+  # # the 10th draw is kept
+  # keepevery_stage2 <- 10
+  # keepevery_stage1 <- 50
+  
+  n=dim(dat)[1]
+  num_trees <- 50
+  sigdf_val <- 3
+  sigquant_val <- 0.9
+  
+  ############################# Stage 2 ###################################
+  X = as.matrix(cbind(dat$A1,dat$A2,dat$X_11,dat$X_21,dat$X_3,dat$L1,dat$X_12,dat$X_22,dat$L2))   
+  y = dat$Y
+  # dat <- as.data.frame(cbind(L1,A1,L2,A2,Y))
+  # dat <- as.data.frame(cbind(L1,A1,L2,A2,X_11,X_21,X_3,X_12,X_22,Y))
+  
+  newdata <- dat
+  newdata$a2_0 <- 0
+  newdata$a2_1 <- 1
+  X_a2_0 <- as.matrix(newdata[,c("A1","a2_0","X_11","X_21","X_3","L1","X_12","X_22","L2")])
+  #X_a2_1 <- as.matrix(newdata[,c("A1","a2_1","L1","L2")])
+  X_a2_1 <- as.matrix(newdata[,c("A1","a2_1","X_11","X_21","X_3","L1","X_12","X_22","L2")])
+  # Initialization
+  newdata$Y_a2_0 <- 0
+  newdata$Y_a2_1 <- 0
+  newdata0 <- newdata[,c(1:9,11,13)]
+  colnames(newdata0)[c(10,11)] <- c("a2","Y_hat")
+  newdata1 <- newdata[,c(1:9,12,14)]
+  colnames(newdata1)[c(10,11)] <- c("a2","Y_hat")
+  # change to long format
+  # Obtain D_aug 
+  data_aug <- as.data.frame(rbind(newdata0,newdata1))
+  #View(data_aug)
+  # X.test for prediction
+  X_test <- as.matrix(cbind(data_aug$A1,data_aug$a2,data_aug$X_11,data_aug$X_21,data_aug$X_3,
+                            data_aug$L1,data_aug$X_12,data_aug$X_22,data_aug$L2))
+  #View(X_test)
+  ## Prior info
+  # k: 2.000000
+  # degrees of freedom in sigma prior: 3 (nu) variable_name = sigdf
+  # quantile in sigma prior: 0.900000 (q) variable_name = sigquant
+  # power and base for tree prior: 2.000000 0.950000
+  
+  ndpost_num_stage_2 = mc_num - burnin
+  bartFit = bart(x.train=X,y.train=y,x.test=X_test,ntree=num_trees, sigdf=sigdf_val,
+                 sigquant=sigquant_val, ndpost=ndpost_num_stage_2,
+                 nskip=burnin, keepevery=keepevery_stage2, verbose=FALSE) 
+  n_preserved_stage2 <- dim(bartFit$yhat.test)[1]
+  
+  ATE_list <- c()
+  
+  for(i in 1:n_preserved_stage2){
+    
+    data_aug$Y_hat <- c(bartFit$yhat.test[i,]+rnorm(length(bartFit$yhat.test[i,]),
+                                                    mean=0,sd=bartFit$sigma[i]))
+    
+    ################################################# Stage 1 ###########
+    X <- as.matrix(cbind(data_aug$A1,data_aug$a2,data_aug$X_11,data_aug$X_21,data_aug$X_3,data_aug$L1))
+    y <- data_aug$Y_hat
+    
+    newdata_aug <- dat
+    newdata_aug$a1_0 <- 0
+    newdata_aug$a1_1 <- 1
+    newdata_aug$a2_0 <- 0
+    newdata_aug$a2_1 <- 1
+    X_a1_0_a2_0 <- as.matrix(newdata_aug[,c("a1_0","a2_0","X_11","X_21","X_3","L1")])
+    X_a1_1_a2_0 <- as.matrix(newdata_aug[,c("a1_1","a2_0","X_11","X_21","X_3","L1")])
+    X_a1_0_a2_1 <- as.matrix(newdata_aug[,c("a1_0","a2_1","X_11","X_21","X_3","L1")])
+    X_a1_1_a2_1 <- as.matrix(newdata_aug[,c("a1_1","a2_1","X_11","X_21","X_3","L1")])
+    X_test_1 = as.matrix(rbind(X_a1_0_a2_0,X_a1_1_a2_0,X_a1_0_a2_1,X_a1_1_a2_1))
+    
+    ndpost_num_stage_1 = mc_num_stage_1 - burnin
+    bartFit_1 = bart(x.train=X,y.train=y,x.test=X_test_1,ntree=num_trees,sigdf=sigdf_val,
+                     sigquant=sigquant_val, ndpost=ndpost_num_stage_1, nskip=burnin,
+                     keepevery=keepevery_stage1, verbose=FALSE) 
+    
+    
+    Y_hat_stage1 <- bartFit_1$yhat.test
+    n_preserved_stage1 <- dim(Y_hat_stage1)[1]
+    
+    for(j in 1:n_preserved_stage1){
+      Y_stage1 <- Y_hat_stage1[j,]
+      new_ATE <- mean(Y_stage1[(3*n+1):(4*n)]) - mean(Y_stage1[1:n])
+      ATE_list <- c(ATE_list, new_ATE)
+    }
+    
+  }
+  return(ATE_list)
+  
+}
+
+M = 100  # number of datasets simulated: 50~500
+n = 500
+true_ATE_Zhu_2023 <- 0.5093185
+est_ATE <- c()
+sd_ATE <- c()
+coverage_num <- 0
+upper_CI <- 0
+lower_CI <- 0
+ATE_list <- c()
+
+for (i in 1:M){
+  seed_num <- i+1000
+  set.seed(seed_num)
+  # At time t = 1, simulate for each individual baseline covariates X_11, X_21, X_3
+  X_11 <- rnorm(n,0,1)
+  X_21 <- rnorm(n,0,1)
+  X_3 <- abs(rnorm(n,0,1))
+  
+  # Generate treatment A_1
+  A0 <- rep(0, n)
+  pi1 <- (exp(-A0+ (1*X_11-0.5*X_21+0.25*X_3) + (-1/2)))/(1+exp(-A0+(1*X_11-0.5*X_21+0.25*X_3)+(-1/2)))
+  A1 <- rbern(n, prob = pi1)
+  
+  # At time t = 2, simulate for each individual their time-varying confounders X_12, X_22
+  U2 <- 2+(2*A1 - 1)/3
+  
+  Z12 <- rnorm(n,0,1)
+  Z22 <- rnorm(n,0,1)
+  
+  X_12 <- Z12*U2
+  X_22 <- Z22*U2
+  
+  # Generate treatment A_2
+  pi2 <- (exp(-A1 + (1*X_12-0.5*X_22) + (-1/2)^2))/(1 + exp(-A1+(1*X_12-0.5*X_22)+(-1/2)^2))
+  A2 <- rbern(n, prob = pi2)
+  
+  Y <- -1-(3/(1+exp(-6*(X_11+X_21+X_3+X_12+X_22)))) + 2*X_12*X_22 +
+    1.8*(X_11+X_21+X_3)*A1 - (2/(1+exp(-4*(X_12+X_22))))*A2 +rnorm(n,0,sd=0.8)
+  
+  # Calculate propensity score
+  # Stage 2
+  x_df_2 <- as.data.frame(cbind(X_11,X_21,X_3,A1,X_12,X_22))
+  sl_lib <- c("SL.glmnet", "SL.glm", "SL.gam")
+  result_2 <- SuperLearner(Y = A2, X = x_df_2, SL.library = sl_lib, method = "method.NNLS", family = binomial())
+  mydata_X_superlearner_predict <- predict(result_2)
+  e2 <- ifelse(A2 == 0, 1-as.numeric(mydata_X_superlearner_predict$pred),
+               as.numeric(mydata_X_superlearner_predict$pred))
+  L2 <- e2
+  
+  # Stage 1
+  x_df_1 <- as.data.frame(cbind(X_11,X_21,X_3))
+  result_1 <- SuperLearner(Y = A1, X = x_df_1, SL.library = sl_lib, method = "method.NNLS", family = binomial())
+  mydata_X_superlearner_predict <- predict(result_1)
+  e1 <- ifelse(A1 == 0, 1-as.numeric(mydata_X_superlearner_predict$pred),
+               as.numeric(mydata_X_superlearner_predict$pred))
+  L1 <- e1
+  
+  # Collect the propensity score and combine the dataset
+  dat <- as.data.frame(cbind(L1,A1,L2,A2,X_11,X_21,X_3,X_12,X_22,Y))
+  
+  
+  ATE_list <- BART_ICE_X_PS(dat)
+  est_ATE[i] <- mean(ATE_list)
+  sd_ATE[i] <- sd(ATE_list)
+  upper_CI <- quantile(ATE_list, 0.975)
+  lower_CI <- quantile(ATE_list, 0.025)
+  
+  print("-----------Seed Number------------------")
+  print(seed_num)
+  
+  c <- 0
+  
+  if((true_ATE_Zhu_2023 <= upper_CI) & (true_ATE_Zhu_2023 >= lower_CI)){
+    coverage_num <- coverage_num+1
+    c <- 1
+  }
+  
+  print("-----------Individual Coverage------------------")
+  print(c)
+  print("--------------Individual ATE---------------")
+  print(est_ATE[i])
+  print("-----------------------------")
+  
+}
+
+# Percentage Bias
+bias_BART <- mean(est_ATE)-true_ATE_Zhu_2023
+percentage_bias_BART <- abs(bias_BART/true_ATE_Zhu_2023)
+paste("(BART_Zhu_X_PS) Percentage Bias: ", percentage_bias_BART)
+
+# Credible Interval Coverage: the proportion of all datasets for which the 95% credible intervals contained the true ATE
+credible_interval_coverage_BART <- coverage_num/M
+paste("(BART_Zhu_X_PS) Credible Interval Coverage: ", credible_interval_coverage_BART)
+
+# RMSE
+BART_RMSE <- sqrt(mean((est_ATE - true_ATE_Zhu_2023)^2))
+paste("(BART_Zhu_X_PS) RMSE: ", BART_RMSE)
+
+# MCSE
+MCSE <- sd(est_ATE)
+paste("(BART_Zhu_X_PS) MCSE: ", MCSE)
+
+# AESE
+AESE <- mean(sd_ATE)
+paste("(BART_Zhu_X_PS) AESE: ", AESE)
+
+
